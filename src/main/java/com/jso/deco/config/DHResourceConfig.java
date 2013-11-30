db8873e4da048016d2995b3d53b6c598bbc0dacd
JSO-Technologies <JSO.Technologies@gmail.com>
2013-11-30 12:28:33 +0100

    Jackson integration

--- src/main/java/com/jso/deco/config/DHResourceConfig.java
+++ src/main/java/com/jso/deco/config/DHResourceConfig.java
@@ -1,5 +1,6 @@
 package com.jso.deco.config;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.jso.deco.MyResource;
@@ -10,6 +11,7 @@ public class DHResourceConfig extends ResourceConfig {
      * Register JAX-RS application components.
      */
     public DHResourceConfig () {
+    	register(JacksonFeature.class);
     	register(MyResource.class);
 //        register(RequestContextFilter.class);
 //        register(JerseyResource.class);
