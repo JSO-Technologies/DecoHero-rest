ffccb7aba6acc207e5226a3fc83f0c7316658013
JSO-Technologies <JSO.Technologies@gmail.com>
2014-01-04 23:00:17 +0100

    TU correction for CI

--- DecoHero-service/src/test/java/com/jso/deco/service/session/CookieEncoderTest.java
+++ DecoHero-service/src/test/java/com/jso/deco/service/session/CookieEncoderTest.java
@@ -9,6 +9,7 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.ws.rs.core.NewCookie;
 
+import org.apache.commons.codec.binary.Base64;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
@@ -50,33 +51,50 @@ public class CookieEncoderTest {
 	}
 	
 	@Test
-	public void cookieToSession_should_deserialize_cookie_value() throws URISyntaxException, IOException {
+	public void cookieToSession_should_deserialize_cookie_value() throws URISyntaxException, IOException, NoSuchAlgorithmException {
 		//given
-		String cookie_value = "dExKcVM3OGp1MnBLOGNxbHBhVnc4L0twaHZ3PTsxMjM7MTM4NzkyNjAwMDAwMA==";
+		long expirationTime = new DateTime(2013, 12, 25, 0, 0, 0).getMillis();
+		String cookieValue = cookieValue("123", expirationTime);
+		String expectedHash = cookieHash("123", expirationTime);
 
 		//when
-		Session resultingSession = cookieEncoder.cookieToSession(cookie_value);
+		Session resultingSession = cookieEncoder.cookieToSession(cookieValue);
 
 		//then
 		assertThat(resultingSession.getUserId()).isEqualTo("123");
-		assertThat(resultingSession.getExpirationTime()).isEqualTo(new DateTime(2013, 12, 25, 0, 0, 0).getMillis());
-		assertThat(resultingSession.getHash()).isEqualTo("tLJqS78ju2pK8cqlpaVw8/Kphvw=");
+		assertThat(resultingSession.getExpirationTime()).isEqualTo(expirationTime);
+		assertThat(resultingSession.getHash()).isEqualTo(expectedHash);
 	}
 
 	@Test
-	public void sessionToCookie_should_serialize_session() throws URISyntaxException {
+	public void sessionToCookie_should_serialize_session() throws URISyntaxException, NoSuchAlgorithmException {
 		//given
-		Session session = new Session("123", new DateTime(2013, 12, 25, 0, 0, 0).getMillis());
+		long expirationTime = new DateTime(2013, 12, 25, 0, 0, 0).getMillis();
+		Session session = new Session("123", expirationTime);
+		
+		String expectedValue = cookieValue("123", expirationTime);
 
 		//when
 		NewCookie resultingCookie = cookieEncoder.sessionToCookie(session, RequestUtils.getMockedRequestContext("http://localhost"));
 
 		//then
 		assertThat(resultingCookie.getName()).isEqualTo(SessionManager.DECO_COOKIE);
-		assertThat(resultingCookie.getValue()).isEqualTo("dExKcVM3OGp1MnBLOGNxbHBhVnc4L0twaHZ3PTsxMjM7MTM4NzkyNjAwMDAwMA==");
+		assertThat(resultingCookie.getValue()).isEqualTo(expectedValue);
 		assertThat(resultingCookie.getDomain()).isEqualTo("localhost");
 		assertThat(resultingCookie.getPath()).isEqualTo("/");
 		assertThat(resultingCookie.getVersion()).isEqualTo(1);
+	}
+	
+	private String cookieValue(String userId, long expirationTime) throws NoSuchAlgorithmException {
+		String hash = cookieHash(userId, expirationTime);
+		String toEncode = hash + ";" + userId + ";" + expirationTime;
+		return new String(Base64.encodeBase64(toEncode.getBytes()));
+	}
+	
+	private String cookieHash(String userId, long expirationTime) throws NoSuchAlgorithmException {
+		String toEncode = expirationTime + ";" + userId + ";decoherodecohero";
+		byte[] sha1 = MessageDigest.getInstance("SHA-1").digest(toEncode.getBytes());
+		return new String(Base64.encodeBase64(sha1));
 	}
 
 }
